fbed1c954802b5ca3a4b884b51de455e52f1adc3
Yesuraj <65080677+yesuraj130@users.noreply.github.com>
2026-06-06 16:27:01 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/app/MainActivity.java
@@ -0,0 +1,54 @@
+package com.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Fullscreen
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+
+        // Core performance settings
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+
+        s.setUseWideViewPort(true);
+        s.setLoadWithOverviewMode(true);
+
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // YOUR LAN DEVICE
+        webView.loadUrl("http://192.168.1.100");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
+}
